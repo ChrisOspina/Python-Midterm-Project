243380539737de13252d640f1e63c274f74b035f
Genius Machado <44523632+GeniusMachado@users.noreply.github.com>
2023-03-14 17:14:19 +0000

    Add files via upload

--- imdb.py
+++ imdb.py
@@ -1,27 +1,28 @@
-def find_null_values(dataset):
-	dataset.isnull().sum().sum()
-	num_null = dataset.isnull().sum()
+def find_null_values(imdb):
+	imdb.isnull().sum().sum()
+	num_null = imdb.isnull().sum()
 	print("No of Null Values: {}".format(num_null))
 
-def average_bat(dataset):
-  total_bat = dataset['batsman_run'].sum()
-  total_bat= len(dataset.index) - 1
+def average_bat(imdb):
+  total_bat = imdb['batsman_run'].sum()
+  total_bat= len(imdb.index) - 1
   avg_runs = total_bat / total_bat
   print('Average of Batsman run for Ball_to_ball is: {}'.format(avg_runs))
 
-def win_total_maches(dataset, win, total_matches):
-  win = dataset.groupby('WinningTeam').count()
-  win
+def duration_rating(imdb, Runtime, IMDB_Rating):
+  im = imdb.groupby('Released_Year').count()
+  imdb
   
-  total_matches = dataset['Team1'].value_counts()+ dataset['Team2'].value_counts()
-  total_matches
+  duration_rating = imdb['Series_Title'].value_counts()+ imdb['Team2'].value_counts()
+  duration_rating
   
-  win['Total matches']=total_matches
-  win[["Total matches","WonBy"]].sort_values(by=["Total matches"],ascending=False).plot.bar(stacked=True,figsize=(7,3))
+  imdb['Total Movies']=total_movies
+  imdb[["Total Duration","IMDB Rated"]].sort_values(by=["Total Movies"],ascending=False).plot.bar(stacked=True,figsize=(7,3))
 
 def runs_scored_ascending(most_runs):
   asc_most_runs = most_runs.sort_values(by="total_run", ascending=True)
   print(asc_most_runs)
+
 
 
 
